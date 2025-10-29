30f576d41e2f213ccfda787b0e4f29fe1f4956ff
Sammyhein <sam.pym@gmail.com>
2025-10-29 11:15:35 +0100

    intégration progress Barre in Code

--- game.js
+++ game.js
@@ -33,6 +33,7 @@ const question = document.getElementById("question-text");
 const options = document.getElementById("options-container");
 const boutonSuivant = document.getElementById('next-button');
 const boutonRejouer = document.getElementById('replay-button');
+const progressBar = document.getElementById("progressBar");
 
 // Fonction pour afficher une question basée sur l'index actuel
 function loadQuestion() {
@@ -70,6 +71,23 @@ boutonSuivant.addEventListener('click', () => {
     boutonSuivant.style.display = 'none'; // Cacher le bouton Suivant
     boutonRejouer.style.display = 'inline-block'
   }
+
+  if ( currentBar < maxBar){                             // Si ma valeur de départ est inférieur à ma valeur maximum
+            currentBar += step                                // alors augmente ma valeur de départ de 25
+            if (currentBar> maxBar) currentBar = maxBar      //Si le nouvel ajout fait passer currentBar au-delà de maxBar, on la remet exactement à maxBar.
+            progressBar.value = currentBar
+        }
+         if (currentBar >= maxBar) {                        // Si ma valeur est supérieure ou égale a ma valeur max (quizz fini)
+                                                          
+          boutonSuivant.disabled = true;                  // Alors, désactive le bouton suivant
+         
+         
+         setTimeout(() => {                               //// Laisse le navigateur mettre à jour la barre avant l'alerte
+
+         
+       alert("Bravo le Quiz est fini !");      
+      }, 200);                                            // J'ai mis 200 ms car ca mes emblait bien mais on peut changer. C'est le temps de rechargement de la. barre de progression avant le lancement de l'alerte. 
+    }
 });
 
 // Charger la première question au chargement de la page
@@ -92,3 +110,13 @@ boutonRejouer.addEventListener('click', () => {
   loadQuestion(currentQuestionIndex)
 
 });
+
+let button = document.getElementById("boutonSuivant")
+
+
+
+let currentBar = 0;
+let maxBar = 100;
+const step = 25
+
+  
